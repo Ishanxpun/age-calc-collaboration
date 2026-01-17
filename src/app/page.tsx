cb949ea5e046225301750653c6b16c225c05cbb6
Ishanxpun <punishan500@gmail.com>
2026-01-17 19:04:09 +0545

    btn added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ const page = () => {
   return (
     <div>
       <nav className='flex h-30 w-screen bg-black text-white justify-between px-4'>
-         
+         <button>
+          click me 
+         </button>
       </nav>
     </div>
   )
