fdc911b1d73bff15828386c7c0a3a2ee8540167f
Gamma <221785314+Coder-Gamma@users.noreply.github.com>
2026-01-17 19:11:10 +0545

    fixed errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,9 @@ import React from "react";
 const page = () => {
   return (
     <div>
-
-      <nav className='flex h-30 w-screen bg-black text-white justify-between px-4'>
-         <button>
-          click me 
-         </button>
-
       <nav className="flex h-30 w-screen bg-black text-white justify-between px-4">
+        <button>click me</button>
+
         <select>
           <option>Nirdosh</option>
           <option value="">Ishanx</option>
